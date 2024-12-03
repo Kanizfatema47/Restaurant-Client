4d19fb3f11ed425f1330b8f1722ed7241b608984
Kaniz Fatema <kanizfatema0184@gmail.com>
2024-12-03 14:17:43 +0600

    MenuItem done

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,16 @@
+import MenuItem from "../../../Shared/MenuItem/MenuItem";
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
+import Featured from "../Featured/Featured";
+import PopularMenu from "../PopularMenu/PopularMenu";
 
 export default function Home() {
   return (
     <div>
       <Banner />
       <Category />
+      <PopularMenu/>
+      <Featured/>
     </div>
   )
 }

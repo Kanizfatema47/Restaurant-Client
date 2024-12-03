7c630e56623b1f85c3eaf507c6108d86b2434173
Kaniz Fatema <kanizfatema0184@gmail.com>
2024-12-03 16:56:15 +0600

    menu cover

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Helmet } from 'react-helmet-async'
-
+import Cover from '../../Shared/Cover/Cover'
+import bannerimg from '../../assets/menu/banner3.jpg'
 const Menu = () => {
     return (
 
@@ -8,6 +9,9 @@ const Menu = () => {
             <Helmet>
                 <title>Restaurant | Menu</title>
             </Helmet>
+            <Cover img={bannerimg}
+            title='Our Menu'
+            />
         </div>
     )
 }
